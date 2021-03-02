fec0f79aadd167b94878d976ad5092180ca866a4
asd <asd>
2021-03-02 01:15:55 -0300

    crearProyecto a components

---
+++ src/components/crearProyecto.vue
@@ -0,0 +1,69 @@
+<template>
+<div class="container">
+    <h1>Agregar un proyecto</h1>
+    <form>
+        <div>
+            <label for="name">Nombre</label>
+            <input type="text" id="nombre" v-model="newProyecto.nombre">
+        </div>
+        <div>
+            <label for="name">Descripcion</label>
+            <input type="text" id="email" v-model="newProyecto.descripcion">
+        </div>
+        <div>
+            <label for="name">Nombre de Compañia</label>
+            <input type="text" id="gender" v-model="newProyecto.nombreCompania">
+        </div>
+        <div>
+            <label for="name">Presupuesto disponible</label>
+            <input type="text" id="phone" v-model="newProyecto.presupuesto">
+        </div>
+        <div>
+            <label for="name">Tiempo Estimado</label>
+            <input type="text" id="phone" v-model="newProyecto.tiempoEstimado">
+        </div>
+        <div>
+            <button type="button" @click="send">Crear Proyecto</button>
+        </div>
+        <div class="info">
+            <h2>Objeto</h2>
+            <code>{{newProyecto}}</code>
+            <p class="message">
+                {{message}}
+            </p>
+        </div>
+    </form>
+</div>
+</template> 
+
+<script>
+export default {
+    data(){
+        return{
+            message:'',
+            newProyecto:{
+            }
+        }       
+    },
+    methods:{
+        send:async function(){
+            this.message = '';
+            if (this.newProyecto.nombre == ''){
+                this.message = 'Debes ingresar un nombre para el proyecto'
+                return false
+            }
+            try {
+                var result = await this.$http.post('/api/proyectos',
+this.newProyecto);
+                let proyecto = result.data;
+                this.message = `Se creó un nuevo proyecto con id:
+${proyecto.data._id}`;
+                this.newProyecto = {};
+            } catch (error) {
+                console.log('error', error)
+                this.message = 'Ocurrió un error'
+            }
+        }
+    }
+}
+</script>
