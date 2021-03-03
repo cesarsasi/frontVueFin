2661d892815cd4e565d47ee1bf20cacf45670aec
asd <asd>
2021-03-03 11:37:03 -0300

    creando vista listar portulantes

--- src/components/listarPostulantes.vue
+++ src/components/listarPostulantes.vue
@@ -1,40 +1,198 @@
 <template>
+  <v-container fluid>
+    <v-data-iterator
+      :items="postulaciones"
+      :items-per-page.sync="itemsPerPage"
+      hide-default-footer
+    >
+      <template v-slot:header>
+        <v-toolbar
+          class="mb-2"
+          color="indigo darken-5"
+          dark
+          flat
+        >
+          <v-toolbar-title>Lista de Postulaciones </v-toolbar-title>
+        </v-toolbar>
+      </template>
 
-<table>
-  <thead>
-    <tr>
-      <th>Nombre</th>
-      <th>Apellido</th>
-      <th>Rut</th>
-      <th>Correo</th>
-      <th>Telefono</th>
-    </tr>
-  </thead>
-  <tbody >
-    <tr>
-      <td>January</td>
-      <td>$100</td>
-      <td>$19145688 - 9</td>
-    </tr>
-    <tr>
-      <td>February</td>
-      <td>$80</td>
-    </tr>
-  </tbody>
-  <tfoot >
-    <tr>
-      <td>Sum</td>
-      <td>$180</td>
-    </tr>
-  </tfoot>
-</table> 
+      <template v-slot:default="props">
+        <v-row>
+          <v-col
+            v-for="postulacion in props.items"
+            :key="postulacion.name"
+            cols="12"
+            sm="6"
+            md="4"
+            lg="3"
+          >
+            <v-card>
+              <v-card-title class="subheading font-weight-bold">
+                {{ postulacion.name }}
+              </v-card-title>
+
+              <v-divider></v-divider>
+
+              <v-list dense>
+                <v-list-item>
+                  <v-list-item-content>Calories:</v-list-item-content>
+                  <v-list-item-content class="align-end">
+                    {{ postulacion.calories }}
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Fat:</v-list-item-content>
+                  <v-list-item-content class="align-end">
+                    {{ postulacion.fat }}
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Carbs:</v-list-item-content>
+                  <v-list-item-content class="align-end">
+                    {{ postulacion.carbs }}
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Protein:</v-list-item-content>
+                  <v-list-item-content class="align-end">
+                    {{ postulacion.protein }}
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Sodium:</v-list-item-content>
+                  <v-list-item-content class="align-end">
+                    {{ postulacion.sodium }}
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Calcium:</v-list-item-content>
+                  <v-list-item-content class="align-end">
+                    {{ postulacion.calcium }}
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Iron:</v-list-item-content>
+                  <v-list-item-content class="align-end">
+                    {{ postulacion.iron }}
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Iron:</v-list-item-content>
+                  <v-list-item-content class="align-end">
+                    {{ postulacion.asd }}
+                  </v-list-item-content>
+                </v-list-item>
+
+                <v-list-item>
+                  <v-list-item-content>Iron:</v-list-item-content>
+                  <v-list-item-content class="align-end">
+                    {{ postulacion.dadsa }}
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+
+              
+            </v-card>
+          </v-col>
+        </v-row>
+      </template>
+
+      <template v-slot:footer>
+        <v-toolbar
+          class="mt-2"
+          color="indigo"
+          dark
+          flat
+        >
+          <v-toolbar-title class="subheading">
+            This is a footer
+          </v-toolbar-title>
+        </v-toolbar>
+      </template>
+    </v-data-iterator>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'ListarPostulantes',
-  props: {
-      msg: String
+  export default {
+    data: () => ({
+      itemsPerPage: 4,
+      postulaciones: [
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          sodium: 87,
+          calcium: '14%',
+          iron: '1%',
+          asd: '1%',
+          dadsa: '1%',
+        },
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          sodium: 87,
+          calcium: '14%',
+          iron: '1%',
+          asd: '1%',
+          dadsa: '1%',
+        },
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          sodium: 87,
+          calcium: '14%',
+          iron: '1%',
+          asd: '1%',
+          dadsa: '1%',
+        },
+        {
+          name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          sodium: 87,
+          calcium: '14%',
+          iron: '1%',
+          asd: '1%',
+          dadsa: '1%',
+        },
+      ],
+    }),
+    methods:{
+        send:async function(){
+            this.message = '';
+            if (this.newProyecto.nombre == ''){
+                this.message = 'Debes ingresar un nombre para el proyecto'
+                return false
+            }
+            try {
+                var result = await this.$http.get('/api/projects',this.newProyecto);
+                let proyecto = result.data;
+                this.message = `Se creó un nuevo proyecto con id:${proyecto.data._id}`;
+                this.newProyecto = {};
+            } catch (error) {
+                console.log('error', error)
+                this.message = 'Ocurrió un error'
+            }
+        }
+    }
+
   }
-}
 </script>
