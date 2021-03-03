7fad0b9e42888299b8db473ef6ea915ebaa814f7
asd <asd>
2021-03-03 01:54:16 -0300

    Modificacion Crear proyecto

--- src/components/crearProyecto.vue
+++ src/components/crearProyecto.vue
@@ -3,24 +3,28 @@
     <h1>Agregar un proyecto</h1>
     <form>
         <div>
-            <label for="name">Nombre</label>
+            <label for="name">Nombre del Proyecto </label>
             <input type="text" id="nombre" v-model="newProyecto.nombre">
         </div>
         <div>
-            <label for="name">Descripcion</label>
-            <input type="text" id="email" v-model="newProyecto.descripcion">
-        </div>
-        <div>
-            <label for="name">Nombre de Compañia</label>
+            <label for="name">Nombre de Compañia </label>
             <input type="text" id="gender" v-model="newProyecto.nombreCompania">
         </div>
         <div>
-            <label for="name">Presupuesto disponible</label>
-            <input type="text" id="phone" v-model="newProyecto.presupuesto">
+            <label for="name">Presentacion </label>
+            <input type="text" id="phone" v-model="newProyecto.presentacion">
         </div>
         <div>
-            <label for="name">Tiempo Estimado</label>
-            <input type="text" id="phone" v-model="newProyecto.tiempoEstimado">
+            <label for="name">Fundamentacion </label>
+            <input type="text" id="phone" v-model="newProyecto.fundamentacion">
+        </div>
+        <div>
+            <label for="name">Condiciones </label>
+            <input type="text" id="phone" v-model="newProyecto.condiciones">
+        </div>
+        <div>
+            <label for="name">Requerimientos Proyecto </label>
+            <input type="text" id="phone" v-model="newProyecto.requerimientosProyecto">
         </div>
         <div>
             <button type="button" @click="send">Crear Proyecto</button>
